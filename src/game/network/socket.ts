f2c1f1b2fd9de9afbab0a6f881d017339acc8dcf
Anshik-02 <your_email@example.com>
2026-02-08 20:14:32 +0530

    Fix- websocekect link

--- src/game/network/socket.ts
+++ src/game/network/socket.ts
@@ -1,6 +1,6 @@
 // game/network/socket.ts
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000", {
+export const socket = io("https://signal-be-m21o.onrender.com", {
   transports: ["websocket"],
 });
